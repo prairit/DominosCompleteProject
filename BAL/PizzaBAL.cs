6f7551b01bf887b6b651fa08decdfe5fd80646a4
prairit <54899228+prairit@users.noreply.github.com>
2020-05-07 00:43:14 +0530

    Uploaded the project in a semi state

---
+++ BAL/PizzaBAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BAL
+{
+    public class PizzaBAL
+    {
+        PizzaDAL pizzaDAL;
+
+        public PizzaBAL()
+        {
+            pizzaDAL = new PizzaDAL();
+        }
+        public List<Menu> GetAllPizzas()
+        {
+            return pizzaDAL.GetAllPizzas();
+        }
+    }
+}
---
+++ DAL/PizzaDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class PizzaDAL
+    {
+        DominosEntities context;
+        public PizzaDAL()
+        {
+            context = new DominosEntities();
+        }
+        public List<Menu> GetAllPizzas ()
+        {
+            var pizza = context.Menus.Include(m => m.Prices).ToList();
+            return pizza;
+        }
+    }
+}
